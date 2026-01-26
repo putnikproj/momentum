a3d39c8a74e41cf6a9b5bad93f058e66d7d495c8
Nikita Putilin <putilin.n.e@gmail.com>
2026-01-26 11:00:48 +0400

    Fixed tests

--- apps/frontend/src/app/app.spec.ts
+++ apps/frontend/src/app/app.spec.ts
@@ -1,20 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { App } from './app';
 import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
+  let httpMock: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App, NxWelcome],
+      providers: [provideHttpClientTesting(),],
     }).compileComponents();
+
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
+
+    fixture.detectChanges();
+
+    httpMock.expectOne(process.env['NX_API_URL'] ?? '').flush({ message: 'Hello API' });
+
     await fixture.whenStable();
+
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
       'Welcome frontend',
     );
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 });
